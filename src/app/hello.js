3d2b8fbb9816d0f24832c3bd9d9993152188524e
benjaminhabert <bhabert@quantmetry.com>
2017-04-30 17:42:41 +0200

    feat: installing angular-bootstrap and testing

--- src/app/hello.js
+++ src/app/hello.js
@@ -2,7 +2,21 @@ angular
   .module('app')
   .component('app', {
     templateUrl: 'app/hello.html',
-    controller: function () {
-      this.hello = 'Hello World!';
-    }
+    controller: helloController
   });
+
+function helloController() {
+  this.hello = 'Coucou le monde!';
+  var unObjet = {
+    title: 'Je suis un titre',
+    text: 'Ce texte pourrait être plus long. Ce texte pourrait être plus long.Ce texte pourrait être plus long.Ce texte pourrait être plus long.Ce texte pourrait être plus long.Ce texte pourrait être plus long.Ce texte pourrait être plus long.',
+    date: '2017-03-24',
+    infos: "pas d'infos pour le moment"
+  };
+  this.news = [];
+  for (var i = 0; i < 20; i++) {
+    var newObject = angular.copy(unObjet);
+    newObject.title += " " + i;
+    this.news.push(newObject);
+  }
+}
